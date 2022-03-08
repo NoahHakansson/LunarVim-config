64dc4653dd760adca8eb18440fb9133b260a6dcb
Noah <nallephu12@gmail.com>
2022-03-08 21:59:50 +0100

    Added vim-fugitive to <leader>gs

--- config.lua
+++ config.lua
@@ -18,20 +18,59 @@ vim.opt.wrap = true
 lvim.log.level = "warn"
 lvim.format_on_save = false
 lvim.colorscheme = "gruvbox-material"
+vim.g.gruvbox_material_background = "hard"
+vim.g.gruvbox_material_palette = "mix"
+
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
 -- lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- unmap a default keymapping
--- lvim.keys.normal_mode[""]
+-- 'jk' for quitting insert mode
+lvim.keys.insert_mode["jk"] = false
+-- 'kj' for quitting insert mode
+lvim.keys.insert_mode["kj"] = false
+-- 'jj' for quitting insert mode
+lvim.keys.insert_mode["jj"] = false
 -- edit a default keymapping
 -- lvim.keys.normal_mode["<C-x>"] = ":q<cr>"
 
--- turn off highlight when pressing Esc
+
+
+-- Turn off highlight when pressing Esc
 vim.cmd("nmap <silent> <Esc> :noh <CR>")
--- yank to system clipboard
+-- Yank to system clipboard
 vim.cmd([[vnoremap <leader>y "+y]])
+-- Remaps for swedish keyboard layout.
+vim.cmd("noremap § $")
+-- Bind vim fugitive to which_key <leader>gs
+lvim.builtin.which_key.mappings["g"] = {
+  name = "Git",
+  j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
+  k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
+  l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+  p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+  r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
+  R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+  s = { "<cmd>G<cr>", "Fugitive" },
+  u = {
+    "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
+    "Undo Stage Hunk",
+  },
+  o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
+  b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+  c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+  C = {
+    "<cmd>Telescope git_bcommits<cr>",
+    "Checkout commit(for current file)",
+  },
+  d = {
+    "<cmd>Gitsigns diffthis HEAD<cr>",
+    "Git Diff",
+  },
+}
+
 
 
 
