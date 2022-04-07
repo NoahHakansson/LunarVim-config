fda816a405701969a5220262011acd2c9482c3fd
Noah Hakansson <noah.hakansson@protonmail.com>
2022-04-07 13:38:15 +0200

    neoclip doesnt work ):

--- config.lua
+++ config.lua
@@ -334,14 +334,19 @@ lvim.plugins = {
     event = "BufRead"
   },
   {"ellisonleao/glow.nvim"},
+  -- {
+  --   "AckslD/nvim-neoclip.lua",
+  --   event = "BufWinEnter",
+  --   setup = function()
+  --     vim.cmd [[packadd telescope.nvim]]
+  --   end,
+  -- },
   {
     "danymat/neogen",
     config = function()
         require('neogen').setup()
     end,
     requires = "nvim-treesitter/nvim-treesitter",
-    -- Uncomment next line if you want to follow only stable versions
-    -- tag = "*"
   },
   {
     "nvim-telescope/telescope-project.nvim",
